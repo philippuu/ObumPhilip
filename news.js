be56a65fabe36eba2e713f05713d83ade931cdb7
Philip <philip@lotusbetaanalytics.com>
2017-08-19 16:40:39 +0100

    display each news in a div with ng-repeat attribute and specifying each data KEY to get the value

--- news.js
+++ news.js
@@ -1,15 +1,12 @@
 var app = angular.module('newsApp', []);
 app.controller('newsCtrl', function($scope, $http) {
     var my_api_key = 'f483fa2a3f714981afbee1a1996545b4';
-    var newsSourses = ["the-times-of-india", "google-news", "bbc-news", "mirror", "reuters", "the-hindu", "the-new-york-times", "the-wall-street-journal"];
+    var newsSourses = ["the-sport-bible", "google-news", "bbc-news", "mirror", "reuters", "the-hindu", "the-new-york-times", "the-wall-street-journal"];
     for(var i = 0; i < newsSourses.length; i++){
         $http.get("https://newsapi.org/v1/articles?source=" + newsSourses[i] + "&apiKey=" + my_api_key)
-        .then( 
-            function(response){
-                $scope.sources = response.data.source;
+        .then(function(response){
+                $scope.sources = response.data.articles;
                 console.log($scope.sources);
-                $scope.sourcesData = response.data.articles;
-                console.log($scope.sourcesData[i]);
             }
          );
     }
