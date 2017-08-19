5689cc6b88dbc40469ec7a815c9bc4d8125a1252
Philip <philip@lotusbetaanalytics.com>
2017-08-19 11:31:33 +0100

    change the name of the app and controller in the news.js, supply the URL of the API + the sources array + the API_KEY and then console out the response from the API

--- news.js
+++ news.js
@@ -1,8 +1,14 @@
-var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope) {
+var app = angular.module('newsApp', []);
+app.controller('newsCtrl', function($scope, $http) {
+    var my_api_key = 'f483fa2a3f714981afbee1a1996545b4';
     var newsSourses = ["the-times-of-india", "google-news", "bbc-news", "mirror", "reuters", "the-hindu", "the-new-york-times", "the-wall-street-journal"];
     for(var i = 0; i < newsSourses.length; i++){
-        $http.get(  ).then(  );
+        $http.get("https://newsapi.org/v1/articles?source=" + newsSourses[i] + "&apiKey=" + my_api_key)
+        .then( 
+            function(response){
+                console.log(response);
+            }
+         );
     }
 
 });
