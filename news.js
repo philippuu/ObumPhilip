afbb2cf9eca9d508b50d680f32b7aae3e9c8b8a0
Philip <philip@lotusbetaanalytics.com>
2017-08-19 05:49:33 +0100

    add a for loop to loop through the newsSources array

--- news.js
+++ news.js
@@ -1,5 +1,8 @@
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope) {
     var newsSourses = ["the-times-of-india", "google-news", "bbc-news", "mirror", "reuters", "the-hindu", "the-new-york-times", "the-wall-street-journal"];
-    
+    for(var i = 0; i < newsSourses.length; i++){
+        
+    }
+
 });
